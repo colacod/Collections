c8703f44b7f9867ada9f834e46397a67c137b625
Daniel Colaco <ddemetrio0@gmail.com>
2017-01-11 21:58:54 -0200

    Aplicando set no modelo

---
+++ src/br/com/alura/AplicandoSetNoModelo/TesteEmptySet.java
@@ -0,0 +1,20 @@
+package br.com.alura.AplicandoSetNoModelo;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class TesteEmptySet {
+
+	public static void main(String[] args) {
+
+		Set<String> nomes = Collections.emptySet();
+		nomes.add("Paulo");
+		
+		//o que acontece aqui?
+		/*
+		  	Exception in thread "main" java.lang.UnsupportedOperationException
+			at java.util.AbstractCollection.add(Unknown Source)
+			at br.com.alura.AplicandoSetNoModelo.TesteEmptySet.main(TesteEmptySet.java:11)
+		 */
+	}
+}
